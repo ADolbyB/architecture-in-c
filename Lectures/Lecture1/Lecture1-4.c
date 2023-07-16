4a2a694eaf61fb941bfebeec3bd0d06779c034d2
Joel Brigida <joel@drtechreps.com>
2023-07-15 22:15:24 -0400

    Add Comments for Add'l Placeholders

--- Lectures/Lecture1/Lecture1-4.c
+++ Lectures/Lecture1/Lecture1-4.c
@@ -11,7 +11,12 @@
  * %lf, %g - double (long float)
  * %c - char
  * %s - string
- * %x - hexadecimal
+ * %x - hexadecimal: 0x123A
+ * %o - octal
+ * %e - scientific notation: 1.2e13
+ * %E - scientific notation: 1.2E13
+ * %p - memory addresses: a POINTER to a memory address location
+ * %% - when you actually need the % sign shown in output :)
 */
 
 #include <stdio.h>
