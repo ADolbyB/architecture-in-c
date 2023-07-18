c57e8fd61025259b622b6e061855bf3ce2f1fd16
Joel Brigida <joel@drtechreps.com>
2023-07-17 21:21:50 -0400

    Expand Example With Better Output & Comments

--- Lectures/Lecture2/Lecture2-4.c
+++ Lectures/Lecture2/Lecture2-4.c
@@ -16,7 +16,7 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>                                     // exit() function
+//#include <stdlib.h>                                     // exit() function
 #include <string.h>
 #include <unistd.h>                                     // Unix processes in C
 #include <sys/wait.h>
@@ -30,7 +30,7 @@ int main()
     if(pipe(fd) == -1)                                  // create a pipe
     {
         printf("Pipe Error! Exiting...\n");
-        exit(1);
+        return 1;
     }
     else
     {
@@ -41,20 +41,21 @@ int main()
     if(pid < 0)                                         // Error Condition
     {
         printf("Fork Failed!!! Exiting...\n");
-        exit(1);
+        return 2;
     }
     else if(pid > 0)                                    // Parent process
     {
         close(fd[0]);                                   // close reading end of pipe
         write(fd[1], "Hello", strlen("Hello") + 1);     // write to the pipe
-        wait(NULL);
         close(fd[1]);
+        wait(NULL);                                     // wait for child process to finish
     }
     else                                                // child process
     {
         close(fd[1]);                                   // Close writing end of pipe
         read(fd[0], buf, sizeof(buf));                  // Read from the pipe
         printf("Received string: %s\n", buf);
+        close(fd[0]);
     }
 
     return 0;
