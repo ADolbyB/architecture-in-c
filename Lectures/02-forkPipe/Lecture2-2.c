38e466e02e57beafa7c58e0d70fadb77d6afb2b2
Joel Brigida <joel@drtechreps.com>
2023-07-17 21:46:10 -0400

    Tracking forks with PIDs

--- Lectures/02-forkPipe/Lecture2-2.c
+++ Lectures/02-forkPipe/Lecture2-2.c
@@ -4,15 +4,6 @@
  * Pipelining, Forks & Parallel Processing:
  * June 13, 2023 Lecture 2-2: using pid() with fork().
  * 
- * C_PLACEHOLDERS:
- * %d - int (same as %i)
- * %ld - long int (same as %li)
- * %f - float
- * %lf, %g - double (long float)
- * %c - char
- * %s - string
- * %x - hexadecimal
- * %p - address
 */
 
 #include <stdio.h>
