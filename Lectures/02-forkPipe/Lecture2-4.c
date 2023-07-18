073acf83ca221ec722cdb3cb7126a59e00d060d3
Joel Brigida <joel@drtechreps.com>
2023-07-17 21:47:10 -0400

    fork(), pipe(), read(), write() Example

--- Lectures/02-forkPipe/Lecture2-4.c
+++ Lectures/02-forkPipe/Lecture2-4.c
@@ -3,16 +3,6 @@
  * CDA 4102: Computer Architecture
  * Pipelining, Forks & Parallel Processing:
  * June 13, 2023 Lecture 2-4: fork(), pipe(), & read/write.
- * 
- * C_PLACEHOLDERS:
- * %d - int (same as %i)
- * %ld - long int (same as %li)
- * %f - float
- * %lf, %g - double (long float)
- * %c - char
- * %s - string
- * %x - hexadecimal
- * %p - address
 */
 
 #include <stdio.h>
