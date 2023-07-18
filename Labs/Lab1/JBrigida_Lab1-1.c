02438d646a74340961119984273788432c6861d4
Joel Brigida <joel@drtechreps.com>
2023-07-18 13:29:36 -0400

    Lab 1.1 Solved: Functioning

--- Labs/Lab1/JBrigida_Lab1-1.c
+++ Labs/Lab1/JBrigida_Lab1-1.c
@@ -17,26 +17,32 @@
 
 int main()
 {
-    int age[3], i = 0, j = 0;
+    int age[3], i, j;
     char name[3][50];
     double salary[3];
 
     for(i = 0; i < 3; i++)  
-    { 
-        printf("Enter the name of employee %d: ", i+1);                         // prompt the to enter name
-        scanf("%s", name[i]);                                                   // scan user’s input
+    {
+        // prompt the to enter age
+        // scan user’s input
+        // prompt the to enter salary
+        // scan user’s input
+        
+        printf("Enter the name of employee %d: ", i + 1);                           // prompt the to enter name
+        scanf("%s", &name[i]);                                                       // scan user’s input
+        
+        printf("Enter the age of employee %d: ", i + 1);                            // prompt the to enter name
+        scanf("%d", &age[i]);                                                        // scan user’s input
 
-        //prompt the to enter age
-        //scan user’s input
-        //prompt the to enter salary
-        //scan user’s input
+        printf("Enter the salary of employee %d: ", i + 1);                         // prompt the to enter name
+        scanf("%lf", &salary[i]);                                                    // scan user’s input
 
-        printf("\n");                                                           // a new line
+        printf("\n");                                                               // a new line
     }
 
     for(j = 0; j < 3; j++)
     {
-        //print Prints information (name, age, salary) of each employee in a separate line 
+        // print Prints information (name, age, salary) of each employee in a separate line 
 
         printf("The name of employee %d is: %s. They are %d years old, and their salary is $%.2f.\n",  j+1, name[j], age[j], salary[j]); 
     }
