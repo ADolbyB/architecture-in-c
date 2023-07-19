0b18b982ef24e9713390c85ff3d2102b02ae94e1
Joel Brigida <joel@drtechreps.com>
2023-07-18 21:58:02 -0400

    1.4: Pointers & Addresses

--- Labs/Lab1/JBrigida_Lab1-4.c
+++ Labs/Lab1/JBrigida_Lab1-4.c
@@ -2,7 +2,7 @@
  * Joel Brigida
  * CDA4102: Computer Architecture
  * July 13, 2023
- * Pref. BAssem Alhalabi
+ * Pref. Bassem Alhalabi
  * Lab 1-4:
  *      Write a C program that:
  *      - Declares a variable and a pointer and assigns a value to the variable.
@@ -15,19 +15,26 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()  
-{ 
-    // declare variable 
-    // declare variable 
-    // declare variable 
+int main()
+{
+    int myValue = 41;
+    int *myPtr;
 
-    pointer = &variable;                       // Assigns the address of the variable to the pointer 
+    myPtr = &myValue;                                       // Assigns the address of the variable to the pointer 
 
-    // Prints the address of the pointer and the value stored
+    printf("\n\nmyPtr points to address: %p\n", myPtr);     // Prints the address of the pointer and the value stored
+    printf("Address of myPtr: %p\n", &myPtr);
+    printf("Value of myValue: %d\n", myValue);
+    printf("Address of myValue: %p\n", &myValue);
+    printf("Retrieve myValue by dereferencing myPtr: %d\n", *myPtr);
 
-    // Changes the value of the variable
+    myValue = 73;                                           // Changes the value of the variable
 
-    // Prints the address and the value of the pointer
-    
+    printf("\n\nmyPtr points to address: %p\n", myPtr);     // Prints the address and the value of the pointer
+    printf("Address of myPtr: %p\n", &myPtr);
+    printf("Value of myValue: %d\n", myValue);
+    printf("Address of myValue: %p\n", &myValue);
+    printf("Retrieve myValue by dereferencing myPtr: %d\n\n\n", *myPtr);
+
     return 0;
-} 
+}
