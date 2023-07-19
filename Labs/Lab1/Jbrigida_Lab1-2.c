1ad833a80348c1f47db4d71dd8099b430edd4c2e
Joel Brigida <joel@drtechreps.com>
2023-07-18 22:01:16 -0400

    1.2: Sum/Avg/Min/Max Review

--- Labs/Lab1/Jbrigida_Lab1-2.c
+++ Labs/Lab1/Jbrigida_Lab1-2.c
@@ -2,7 +2,7 @@
  * Joel Brigida
  * CDA4102: Computer Architecture
  * July 13, 2023
- * Pref. BAssem Alhalabi
+ * Pref. Bassem Alhalabi
  * Lab 1-2:
  *      Write a C program that:
  *      - Prompts the user for 6 numbers.
@@ -18,32 +18,46 @@
 
 int main()
 { 
-    int numbers[6], i = 0, sum = 0, minimum = 0, maximum = 0;
+    int numbers[6];
+    int i = 0, sum = 0, minimum = 0, maximum = 0;
     double average = 0;
  
+    printf("\n\nEnter 6 Integers\n");
     for(i = 0; i < 6; i++) 
     {
-        // prompt the to enter a number
-        // scan the number
+        printf("Enter Integer %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+    }
+
+    for(i = 0; i < 6; i++)                                              // compute the sum and average 
+    {
+        sum += numbers[i];                                              // Calculate Sum
     }
     
-    // compute the sum and average                                                                                    
+    double arraySize = (sizeof(numbers) / sizeof(int));                 // get array size
+    //printf("arraySize = %0.1lf\n", arraySize);                        // debug
 
-    minimum = numbers[0];                                                // compute the min and max 
+    average = sum / arraySize;                                          // calculate average
+    minimum = numbers[0];
+    maximum = numbers[0];
 
-    for(i = 0; i < 6; i++)  
-    {  
+    for(i = 0; i < 6; i++)                                              // iterate once to find min & max
+    {
         if(numbers[i] < minimum)
         {
             minimum = numbers[i];
-        }      
+        }
+
+        if(numbers[i] > maximum)      
+        {
+            maximum = numbers[i];
+        }
     }
-    
-    // compute the min and max                                                                                 
-    // print minimum 
-    // print maximum 
-    // print average 
-    // print sum 
+
+    printf("Sum = %d\n", sum);
+    printf("Average = %.3lf\n", average);
+    printf("Maximum = %d\n", maximum);
+    printf("Minimum = %d\n", minimum);
 
     return 0;
 }
