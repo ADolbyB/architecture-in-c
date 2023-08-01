affb6ffb6bb7c01b4c6a5a726a1fb463fd748002
Joel Brigida <joel@drtechreps.com>
2023-07-31 23:43:25 -0400

    SegFault Debug using GDB

---
+++ Examples/gdbDebug/main.c
@@ -0,0 +1,31 @@
+/**
+ * Joel Brigida
+ * CDA 4102: Computer Architecture
+ * 
+ * This is a tutorial on GDB Debugger.
+ * Reference: https://www.youtube.com/watch?v=Dq8l1_-QgAc
+ * 
+ * command format:
+ * `gcc -o [Binary fileName] [Source.c filename] -g`
+ * `gcc -o main main.c -g
+ * 
+ * other useful commands:
+ * `wc -c [Binary fileName]`     // shows size of binary file
+ * `file [Binary fileName]`      // shows elf-file details (should say 'with debug_info')
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int d = 2;
+    printf("Enter an integer to trigger a crash!! ");
+    
+    // scanf("%d", d); // crashes
+    scanf("%d", &d);
+
+    printf("You Entered %d\n\n", d);
+
+    return 0;
+}
