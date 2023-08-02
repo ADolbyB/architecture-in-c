9c9021ff23d0ec35789aafe78bc90d064a23ed3a
Joel Brigida <joel@drtechreps.com>
2023-08-01 22:32:02 -0400

    Add Add'l Source To Description

--- Examples/gdbDebug/main.c
+++ Examples/gdbDebug/main.c
@@ -4,6 +4,7 @@
  * 
  * This is a tutorial on GDB Debugger.
  * Reference: https://www.youtube.com/watch?v=Dq8l1_-QgAc
+ * Ref: https://www.youtube.com/watch?v=MTkDTjdDP3c
  * 
  * command format:
  * `gcc -o [Binary fileName] [Source.c filename] -g`
